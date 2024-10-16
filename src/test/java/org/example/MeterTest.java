8d0e606e039e07c0eb41a662a7eb5ac6f0ab9c2a
renu.bansal <renu.bansal@rea-group.com>
2024-10-16 13:39:35 +0530

    Iteration 1: Add another test case

--- src/test/java/org/example/MeterTest.java
+++ src/test/java/org/example/MeterTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class MeterTest {
     @Test
@@ -11,5 +12,13 @@ public class MeterTest {
         Meter anotherThousandMeter = new Meter(1000);
 
         assertEquals(thousandMeter, anotherThousandMeter);
+    }
+
+    @Test
+    void shouldAssert1MetresIsEqualTo1000Metres() {
+        Meter oneMeter = new Meter(1);
+        Meter thousandMeter = new Meter(1000);
+
+        assertNotEquals(oneMeter, thousandMeter);
     }
 }
